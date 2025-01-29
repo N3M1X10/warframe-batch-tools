b98d1ae508a868de3a483093f3e7b4547b5ed558
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-01-29 21:00:09 +0500

    del src test

--- src/aleca-frame/start-Aleca-Frame.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "G:\Program Files (x86)\Tools\overwolf\" "G:\Program Files (x86)\Tools\overwolf\OverwolfLauncher.exe" -launchapp afmcagbpgggkpdkokjhjkllpegnadmkignlonpjm -from-startmenu"
--- src/kill-warframe.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001>nul
->nul taskkill /f /im "Launcher.exe" /t
->nul taskkill /f /im "Warframe.x64.exe" /t
-exit
