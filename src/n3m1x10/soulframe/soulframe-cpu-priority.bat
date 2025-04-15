913f477981d09cd03edbd8d3d26192a3c0a79ebb
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-04-16 00:12:42 +0500

    reset to old version

--- src/n3m1x10/soulframe/soulframe-cpu-priority.bat
+++ src/n3m1x10/soulframe/soulframe-cpu-priority.bat
@@ -17,17 +17,53 @@ set "arg=%1"
 if "%arg%" == "admin" (
     echo ! Restarted with admin rights and hidden
 ) else (
-    powershell -Command "Start-Process 'cmd.exe' -ArgumentList '/k \"\"%~f0\" admin\"' -Verb RunAs -WindowStyle Minimized"
+    powershell -Command "Start-Process 'cmd.exe' -ArgumentList '/k \"\"%~f0\" admin\"' -Verb RunAs -WindowStyle Hidden"
     exit
 )
 
 :: checking cycles
 
+:: cycle limitation
+set cycle_amount=60
+:: Executable name we looking for
+set process=Soulframe.x64.exe
+:: Variable that makes this cycle doesn't endless, for performance reason
+set cycle_count=1
+
+:cycle
+cls
+echo ! Waiting for the "%process%" launch to change the priority of the process . . .
+echo Cycle Limitation. After %cycle_amount% attempts batch will stopped. Viewing Attempts: %cycle_count%
+
+if %cycle_count% geq %cycle_amount% (exit)
+set /a cycle_count=%cycle_count%+1
+
+:check-step1
+echo Step 1
+>nul timeout /t 4 /nobreak
+tasklist |>nul findstr /b /l /i /c:%process% && goto check-step2
+goto cycle
+:check-step2
+echo Step 2
+>nul timeout /t 4 /nobreak
+tasklist |>nul findstr /b /l /i /c:%process% && goto check-step3
+goto cycle
+:check-step3
+echo Step 3
+>nul timeout /t 5 /nobreak
+tasklist |>nul findstr /b /l /i /c:%process% && goto check-step4
+goto cycle
+:check-step4
+echo Step 4
+>nul timeout /t 6 /nobreak
+tasklist |>nul findstr /b /l /i /c:%process% && goto set-priority
+goto cycle
 
 :: Powershell command that sets priority for process
 :set-priority
-powershell -Command "do {Start-Sleep -Seconds 2; $proc = Get-Process -Name "Soulframe.x64" -ErrorAction SilentlyContinue; $hasWindow = $false; if ($proc -and $proc.MainWindowHandle -ne 0) { $hasWindow = $true; Write-Host "Process has been found"; $proc.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::!priority!; Write-Host "Priority for Soulframe.x64.exe has been changed"} else{cls; Write-Host "Process Soulframe.x64.exe has no window"; Write-Host "Im trying to find him again . . ."}} while (-not $hasWindow)"
+powershell (Get-Process -name "Soulframe.x64").PriorityClass = [System.Diagnostics.ProcessPriorityClass]::%priority%
 echo ! Changing priority complete
+>nul timeout /t 1 /nobreak
 
 :: Source: https://github.com/N3M1X10/warframe-batch-tools
 endlocal
