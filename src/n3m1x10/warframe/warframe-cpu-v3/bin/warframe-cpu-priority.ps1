b5ad6a8e95dc2dc4391da4e7e38ecb4e68d2f03d
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-04-18 18:34:05 +0500

    Update warframe-cpu-priority.ps1

--- src/n3m1x10/warframe/warframe-cpu-v3/bin/warframe-cpu-priority.ps1
+++ src/n3m1x10/warframe/warframe-cpu-v3/bin/warframe-cpu-priority.ps1
@@ -1,3 +1,5 @@
+
+# Params 
 $GameProcName = "Warframe.x64"
 $ClientProcName = "Warframe Launcher"
 $Priority = "high"
@@ -6,12 +8,14 @@ function FindLauncher{
     param(
         [string]$LauncherDescName
     )
+
+    #find all processes named launcher and catching errors
     try {
         $ClientProc = Get-Process -Name "Launcher" -ErrorAction Stop
     }
     catch {
         cls
-        Write-Host "Process '$LauncherDescName' not found"
+        Write-Host "Processes 'Launcher' are not found"
         return $false
     }
 
@@ -24,50 +28,71 @@ function FindLauncher{
     return $false
 }
 
+
+# find process and priority application
+
 function PriorityApplication{    
     param(
         [string]$GameProcName,
         [string]$ClientProcName,
         [string]$Priority
     )
-
+    #priority table :)
+    $priorityMapping = @{
+        'idle' = [System.Diagnostics.ProcessPriorityClass]::Idle
+        'low' = [System.Diagnostics.ProcessPriorityClass]::Low
+        'BelowNormal' = [System.Diagnostics.ProcessPriorityClass]::BelowNormal
+        'normal' = [System.Diagnostics.ProcessPriorityClass]::Normal
+        'AboveNormal' = [System.Diagnostics.ProcessPriorityClass]::AboveNormal
+        'high' = [System.Diagnostics.ProcessPriorityClass]::High
+        'realtime' = [System.Diagnostics.ProcessPriorityClass]::RealTime
+    }
     $BreakOccured = $false
     $HasWindow = $false
+
     do {
         Start-Sleep -Seconds 1
         $GameProc = Get-Process -Name $GameProcName -ErrorAction SilentlyContinue
         $HasLauncher = FindLauncher -LauncherDescName $ClientProcName
-        if ($HasLauncher){
-            if($GameProc) {
-                if ($GameProc -and $GameProc.MainWindowHandle -ne 0) {
-                    $HasWindow = $true
-                    $BreakOccured = $false
-                    Write-Host "Process has been found"
-                    $GameProc.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::$Priority
-                    Write-Host "Priority for $GameProcName.exe has been changed on Priority"
-                }
-                else{
-                    cls
-                    Write-Host "Process Warframe.x64.exe has no window"
-                    Write-Host "Im trying to find him again . . ."
-                }
-            } 
-            else{
-                cls
-                Write-Host "Process Warframe.x64.exe not found"
-                Write-Host "Please, launch Warframe"
-            }
+
+        #If launcher found, but not game process
+        if ($HasLauncher -and -not $GameProc){
+            cls
+            Write-Host "Process Warframe.x64.exe not found"
+            Write-Host "Please, launch Warframe"
         }
-        else{
+
+        #If process window and game process was found, but not launcher
+        elseif (-not $HasLauncher -and $GameProc -and $GameProc.MainWindowHandle -ne 0){
+            $HasWindow = $true
+            $BreakOccured = $false
+            Write-Host "Process has been found"
+            $PriorityClass = $priorityMapping[$Priority.ToLower()]
+            $GameProc.PriorityClass = $PriorityClass
+            Write-Host "Priority for $GameProcName.exe has been changed on $Priority"
+        }
+
+        #If launcher and game process was found, but process hasn't window
+        elseif ($HasLauncher -and $GameProc -and -not ($GameProc.MainWindowHandle -ne 0)){
+            cls
+            Write-Host "Process Warframe.x64.exe has no window"
+            Write-Host "Im trying to find him again . . ."
+        }
+
+        #If launcher and game process are not found
+        elseif (-not $HasLauncher -and -not $GameProc){
             Write-Host "Launcher not found. Script termination..."
             $BreakOccured = $true
             break
         }
+
     } while (-not $HasWindow)
 
     return $BreakOccured
 }
 
+
+# launcher launch check (Don't touch what works) xD
 do{
     Start-Sleep -Seconds 1
     $HasLauncher = FindLauncher -LauncherDescName $ClientProcName
@@ -82,9 +107,13 @@ do{
 
 $BreakFlag = PriorityApplication -GameProcName $GameProcName -ClientProcName $ClientProcName -Priority $Priority
 
+
+# interrupt check (trash part)
 if ($BreakFlag){
     Write-Host "Script was interrupted"
 }
 else{
     Write-Host "Script completed successfully"
 }
+
+Read-Host 
